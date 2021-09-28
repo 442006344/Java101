521ba582d1599384b6aa57492914d9aee1dc358d
Yuosef Al-zahranie <91210074+442006344@users.noreply.github.com>
2021-09-28 09:31:24 +0300

    Update assignment11.java

--- assignment11.java
+++ assignment11.java
@@ -1,10 +1,26 @@
-
 import java.util.Scanner;
-//Your Name Yusef Alzahrani
-//Your ID 442006344
+
+/*
+Ask the user to enter two float digits. For example ( 3.5 and 11.25 )
+Print the calculation for the two digits in separate lines:
+Addition +
+Subtraction  -
+Multiplication  *
+Division /
+Modulus %
+Grater >
+Grater or equal
+Less 
+Less or equal
+Not equal
+Equal
+*/
+
+//Your Name
+//Your ID
 class Main {
-public static void main(String[] args) {
-      //Start your code
-      //another code
-      }
-      }
+  public static void main(String[] args) {
+      //Start your code 
+      
+  }
+}

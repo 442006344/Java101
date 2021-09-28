b7a64a084d7ad8687ad951181ca8c4249f6fbe92
Yuosef Al-zahranie <91210074+442006344@users.noreply.github.com>
2021-09-28 09:10:20 +0300

    Create assignment11.java

---
+++ assignment11.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+//Your Name Yusef Alzahrani
+//Your ID 442006344
+class Main {
+public static void main(String[] args) {
+      //Start your code
+      //another code
+      }
+      }

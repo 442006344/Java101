5497108bf1b24239e7de133c394da8bd0ee62035
Yuosef Al-zahranie <91210074+442006344@users.noreply.github.com>
2021-10-20 21:02:22 +0300

    Update Mid.java

--- Mid.java
+++ Mid.java
@@ -1,1 +1,23 @@
-
+import java.util.Scanner;
+public class Mid {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+  int name = Yuosef;
+           name = scan.nextInt();
+           
+  int birth = 2003;
+          birth = scan.nextInt();
+          System.out.prntln("Yuosef && 2003");
+   
+     int A = 2021 - birth;
+             A  = scan.nextInt();
+             
+     if (A >= 40);
+       System.out.prntln(name);
+     System.out.prntln(A);
+     
+    else if (A>=30 && A<=39 );
+            System.out.prntln(name);
+     System.out.prntln(A);
+     
+     else

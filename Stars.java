426d2b74d06a7478b89728a8892859d669682923
Yuosef Al-zahranie <91210074+442006344@users.noreply.github.com>
2021-12-14 11:47:46 +0300

    Create  Stars.java

---
+++ Stars.java
@@ -0,0 +1,16 @@
+public class Stars {
+public static void main(String[] args) {
+        int p = 0;
+        while(p <= 5){
+        int g = 1;
+        while(g <= p){
+        System.out.print("*");
+        ++g;
+        }
+
+        System.out.println("*");
+
+        ++p;
+        }
+        }
+        }

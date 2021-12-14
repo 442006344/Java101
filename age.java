b525265297520f8f421fe0efe643301c9c0eb770
Yuosef Al-zahranie <91210074+442006344@users.noreply.github.com>
2021-12-14 11:50:20 +0300

    Create age.java

---
+++ age.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class  age.java {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int y;
+        System.out.println("Enter your age to calculate your date of birth");
+        y= scan.nextInt();
+        System.out.println(2021-y);
+
+
+    }
+}

35ce7fd04eff9155cd3afc8420748d64b6e12554
Yuosef Al-zahranie <91210074+442006344@users.noreply.github.com>
2021-12-14 11:54:02 +0300

    Create even switch.java

---
+++ even switch.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class even switch.java {
+public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number 1");
+
+        int v= scan.nextInt();
+        switch (v){
+        case 1:
+
+        int n;
+        System.out.print("Enter a Number");
+        n = scan.nextInt();
+        n %=2;
+        if (n==0){
+        System.out.print("even");
+        }
+        else {
+        System.out.print("odd");
+        }

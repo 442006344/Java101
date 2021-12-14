4e9ecdc113bdd566812a18c9c46a66b79bc7b570
Yuosef Al-zahranie <91210074+442006344@users.noreply.github.com>
2021-12-14 12:06:57 +0300

    Update age.java

--- age.java
+++ age.java
@@ -4,7 +4,7 @@ public class  age.java {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int y;
-        System.out.println("Enter your age to calculate your date of birth");
+        System.out.println("Enter age ");
         y= scan.nextInt();
         System.out.println(2021-y);
 

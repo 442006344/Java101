b1d5f09b7c364df140727598b2fa33d4a5c21ccb
Yuosef Al-zahranie <91210074+442006344@users.noreply.github.com>
2021-12-14 22:03:25 +0300

    Create readWordByWord.java

---
+++ readWordByWord.java
@@ -0,0 +1,10 @@
+public class readWordByWord.java {
+        public static void main(String[] args) {
+                String yyy = "waleed\nalshrhri";
+
+                System.out.println(yyy);
+
+
+
+        }
+}

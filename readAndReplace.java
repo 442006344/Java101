49ac27e5aa5e2f41894abb9796d55352886c1cef
Yuosef Al-zahranie <91210074+442006344@users.noreply.github.com>
2021-12-15 17:39:12 +0300

    Create readAndReplace.java

---
+++ readAndReplace.java
@@ -0,0 +1,11 @@
+public class Main{
+public static void readAndReplace.java(String[] args) {
+     String name = "ABCDBEBGB";
+     for (int x =0;  x<=8 ; ++x){
+         if(name.charAt(x) == 'B'){
+        System.out.println('x');}
+        else {
+            System.out.println(name.charAt(x));
+        }
+     }}}
+    
